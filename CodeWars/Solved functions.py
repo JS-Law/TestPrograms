37fb81e56fc993c721d84a43712fadc3d5124f09
Jeffrey Stanley <stanley.l.jeff@gmail.com>
2023-01-23 17:30:30 -0500

    codewars
    exercise added

---
+++ CodeWars/Solved functions.py
@@ -0,0 +1,16 @@
+'''
+def hoop_count(n):
+    if n >= 10 :
+        return "Great, now move on to tricks"
+    else:
+        return 'Keep at it until you get it'
+'''
+'''
+def hero(bullets, dragons):
+    if dragons * 2 == bullets:
+        return True
+    elif dragons * 2 < bullets:
+        return True
+    else:
+        return False
+'''
