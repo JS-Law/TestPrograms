18be6c6bbab9be12e578ddd8453ccb39111bdc77
Jeffrey Stanley <stanley.l.jeff@gmail.com>
2023-01-23 14:44:38 -0500

    reorg

---
+++ Exercises/blastoff.py
@@ -0,0 +1,11 @@
+n = 12
+while n > 0:
+    print(n) 
+    n = n - 1
+print('BLASSSSSST OFFFFF')
+
+#another way to write this code is with a for loop, learned in W7 5.2
+
+for i in [5 ,4 ,3 ,2 ,1]
+    print(i)
+print('Blast Off!')
---
+++ Exercises/hw.py
@@ -0,0 +1,12 @@
+name = input("Hey whats your name?")
+sname = name.lower()
+if name == "samantha":
+    print("Hello, " + sname + "!")
+    if sname == "samantha":
+        lname = input("whats your last name?")
+        if lname == "ramey":
+            lname = lname.lower()
+            print("Well hello " + sname + " " + lname + " the love of my life, the woman who has my heart forever!!")
+
+else:
+    print("I dont know you!!!")
