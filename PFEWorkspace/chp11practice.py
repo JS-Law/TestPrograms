60b3d8b2a5726f749fd23b4dde3c8722d78ce78d
Jeffrey Stanley <stanley.l.jeff@gmail.com>
2023-02-05 11:33:59 -0500

    Chapter 11
    Added notes on regex and practice problem.

--- PFEWorkspace/chp11practice.py
+++ PFEWorkspace/chp11practice.py
@@ -3,13 +3,27 @@
 # Read through file, look for all integers using re.findall(), then converting the extracted strings
 # to integers
 
-# Open File for Read
 
-name = input("Enter file:")
-if len(name) < 1:
-    name = "regexsum.txt"
-handle = open(name)
+# Import Regular Expression Module
+import re
+
+sum = 0
+#numlist = []
+# Open File for Read, testing for incorrect file name.
+fname = input('Enter file: ')
+try:
+    fhand = open(fname)
+except FileNotFoundError:
+    print('File cannot be opened: ', fname)
+    exit()
 
 # Read Through File Looking for Integers
-for line in handle:
+for line in fhand:
     line = line.rstrip()
+    numlist = re.findall('[0-9]+', line)
+    for numbers in numlist:
+        sum = sum + int(numbers)
+
+print(sum)
+
+
